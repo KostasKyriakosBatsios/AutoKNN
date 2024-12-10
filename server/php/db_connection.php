4b86a071e754b95d54a00314f0e5a70229032fdb
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2024-12-10 17:32:51 +0200

    Commenting what it's unecessary

--- server/php/db_connection.php
+++ server/php/db_connection.php
@@ -3,14 +3,13 @@
     
     $host = $REMOTE_HOST;
     $db = $DB_SCHEMA;
-
-    $userlocal = $DB_USER_LOCAL;
     $user = $DB_USER;
     $pass = $DB_PASS;
 
     $mysqli = new mysqli($host, $user, $pass, $db);
 
 /*  $host = "localhost";
+    $userlocal = $DB_USER_LOCAL;
     $pass = "";
     $db = "autoknn_db";
     $mysqli = new mysqli($host, $userlocal, $pass, $db); */
